bdfea8f094f8966e97000b6d2dfd06887e70dd7a
Rince Mathew <rincemathewmanattu@gmail.com>
2026-05-06 16:40:12 +0530

    layout outer set

---
+++ src/components/main_content.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainContent() {
+  return (
+    <div>
+        <h1>We Listen, We Care, We Heal. </h1>
+        <h6>Empowering Your Health Journey with Compassionate Care and Innovative Solutions.</h6>
+    </div>
+  )
+}
---
+++ src/components/nav_bar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+        <div>Logo</div>
+        <div>Home</div>
+        <div>Products</div>
+        <div>Services</div>
+        <div>About Us</div>
+        <div>Contact Us</div>
+    </div>
+  )
+}
