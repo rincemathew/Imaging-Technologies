114399d26ff3c447d87987cad70007853498877b
Rince Mathew <rincemathewmanattu@gmail.com>
2026-05-13 17:07:42 +0530

    nav gap

--- src/components/nav_bar.tsx
+++ src/components/nav_bar.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <div className='flex items-center justify-center bg-amber-200 w-full'>
-      <div className='flex items-center w-5/6 justify-between px-4 py-8'>
+    <div className='flex items-center justify-center w-full'>
+      <div className='flex items-center w-5/6 justify-between px-3 py-8'>
         <div className='flex items-center'>
           <div>
-            <Image src='/images/logo.png' alt='Logo' width={100} height={100} />
+            <Image src='/images/logo.png' alt='Logo' width={35} height={35} />
           </div>
           <div>imaging<br/>TECHNOLOGY</div>
         </div>
-        <div className='flex items-center'>
+        <div className='flex items-center gap-3'>
           <div>Home</div>
           <div>Products</div>
           <div>Services</div>
