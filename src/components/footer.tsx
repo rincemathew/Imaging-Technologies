de84f281df6e3c8ef69f25fa8f0dfa838ce56987
Rince Mathew <rincemathewmanattu@gmail.com>
2026-05-17 22:15:56 +0530

    footer layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>footer</div>
+    <div className='flex items-center'>
+      <div className='flex items-center justify-center w-full class="h-24"'>
+        <div className='flex flex-col items-center w-5/6 justify-between py-8'>
+       
+        <div className='flex items-center w-full'>
+          <div className='w-1/3'>
+            hello
+          </div>
+          <div className='w-1/3'>
+            hello
+          </div>
+          <div className='w-1/3'>
+            hello
+          </div>
+        </div>
+        <div>
+          <div>
+          ©2026 imaging technology All Rights Reserved.
+          </div>
+          <div>
+            facebook insta etc
+          </div>
+          
+        </div>
+        </div>
+        </div>
+
+    </div>
   )
 }
