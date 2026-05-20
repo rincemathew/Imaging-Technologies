fe37e4090cb43a8f9decebf8afc86c4b3be49165
Rince Mathew <rincemathewmanattu@gmail.com>
2026-05-20 18:06:43 +0530

    header section image change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MainContent from "@/components/main_content";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen">
+    <div className="flex flex-col items-center justify-center w-full">
       <MainContent />
     </div>
   );
--- src/components/main_content.tsx
+++ src/components/main_content.tsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 
 export default function MainContent() {
   return (
-    <div>
+    <div className='w-full'>
       {/* Hero Section */}
-      <div className='flex items-center justify-center w-full class="h-24 bg-gradient-to-tr from-cyan-500 to-blue-500"'>
-        <div className='flex items-center w-5/6 justify-between py-8'>
-          <div className='w-1/2'>
-            <div className='text-4xl font-bold'>Empowering Doctors with the Right Imaging Technology </div>
+      <div className='flex items-center justify-center w-full bg-gradient-to-l from-[#b8c6f4] to-[#fff]'>
+        <div className='md:flex items-center w-full md:w-5/6 justify-between py-8'>
+          <div className='md:w-1/2'>
+            <div className='text-6xl font-bold'>Empowering Doctors with the Right Imaging Technology. </div>
             <div className='text-lg text-gray-700 mt-4'>Authorized dealer of Konica Minolta & Medion Healthcare Pvt Ltd Delivering Transparent Consultation. Reliable Equipment. Long-term Support.</div>
           </div>
-          <div className='w-1/2'>
-            <Image src='/images/about-us-main.png' alt='Logo' width={0} height={0} sizes='100vw' className='w-full h-auto' />
+          <div className='md:w-1/2'>
+            <Image src='/images/home-page-title.png' alt='Logo' width={0} height={0} sizes='100vw' className='w-full h-auto' />
           </div>
         </div>
 
       </div>
       {/* Who We Are */}
-      <div className='flex items-center justify-center w-full class="h-24"'>
+      <div className='flex items-center justify-center w-full'>
         <div className='flex items-center w-5/6 justify-between py-8'>
           <div className='w-1/2'>
-            <Image src='/images/about-us-main.png' alt='Logo' width={0} height={0} sizes='100vw' className='w-full h-auto' />
+            <Image src='/images/home-page-title.png' alt='Logo' width={0} height={0} sizes='100vw' className='w-full h-auto' />
           </div>
           <div className='w-1/2'>
             <h1>Imaging Technology is a Kerala-based medical imaging solutions provider specializing in:</h1>
@@ -36,7 +36,7 @@ export default function MainContent() {
       </div>
 
       {/* Our Brands */}
-      <div className='flex items-center justify-center w-full class="h-24"'>
+      <div className='flex items-center justify-center w-full'>
         <div className='flex items-center w-5/6 justify-between py-8'>
           <h1>Our Brands</h1>
           <div className='grid grid-cols-4 gap-4 w-full'>
@@ -56,7 +56,7 @@ export default function MainContent() {
         </div>
       </div>
       {/* Why Choose Imaging Technology? */}
-      <div className='flex items-center justify-center w-full class="h-24"'>
+      <div className='flex items-center justify-center w-full'>
         <div className='flex items-center w-5/6 justify-between py-8'>
           <h1>Why Choose Us?</h1>
           🔷 Our Process
@@ -80,7 +80,7 @@ export default function MainContent() {
       </div>
 
       {/* Who We Serve */}
-      <div className='flex items-center justify-center w-full class="h-24"'>
+      <div className='flex items-center justify-center w-full'>
         <div className='flex items-center w-5/6 justify-between py-8'>
           <h1>Who We Serve </h1>
           <div className='grid grid-cols-4 gap-4 w-full'>
